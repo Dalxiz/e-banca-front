3b6573d82f9a3c1b8853b020fb863dc2f1381036
dm.ambu <dm.ambu@gmail.com>
2023-04-01 14:22:02 -0300

    sidebar y panel principal

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <sidebar/>
     </nav>
     <router-view/>
   </div>
 </template>
+
+<script>
+
+import Sidebar from './components/Sidebar.vue';
+
+export default {
+  components: { 
+    Sidebar 
+  },
+
+}
+</script>
 
 <style>
 #app {
@@ -15,18 +26,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
